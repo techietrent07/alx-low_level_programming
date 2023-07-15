af47464b9d27b089b33e0e17e2e5fc10c11a06ff
techietrent07 <augustine.onu.200506@unn.edu.ng>
2023-07-14 18:44:53 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char e;
+
+	char f;
+
+	e = 'a';
+	f = 'A';
+	while
+		(e <= 'z') {
+			putchar(e);
+			e++;
+		}
+	while
+		(f <= 'z') {
+			putchar(f);
+			f++;
+		}
+	putchar('\n');
+	return (0);
+}
