c3fe142ff6ee87d75f36f6c35e2a3c2be4d7a494
techietrent07 <augustine.onu.200506@unn.edu.ng>
2023-07-14 20:26:59 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char t;
+
+	t = 'z';
+	while
+		(t >= 'a') {
+			putchar(t);
+			t--;
+		}
+	putchar('\n');
+	return (0);
+}
