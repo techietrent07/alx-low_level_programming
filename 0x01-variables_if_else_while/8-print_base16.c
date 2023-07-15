e89a7149c077bd9fefb5bd7c5ceb6397f08ad44b
techietrent07 <augustine.onu.200506@unn.edu.ng>
2023-07-14 20:36:31 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char t;
+
+	int u;
+
+	t = 'a';
+	u = 0;
+	while
+		(u < 10) {
+			putchar(u + '0');
+			u++;
+		}
+	while
+		(t <= 'f') {
+			putchar(t);
+			t++;
+		}
+	putchar('\n');
+	return (0);
+}
