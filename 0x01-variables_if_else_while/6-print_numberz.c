cad11af2275c60e24b4dd99acf292e3f3d846324
techietrent07 <augustine.onu.200506@unn.edu.ng>
2023-07-14 20:18:37 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int t;
+
+	t = 0;
+	while
+		(t < 10) {
+			putchar(t + '0');
+			t++;
+		}
+	putchar('\n');
+	return (0);
+}
