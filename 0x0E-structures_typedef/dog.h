48da43651e5bb2c630392ba0debf0b4febd2b08d
techietrent07 <augustine.onu.200506@unn.edu.ng>
2023-08-14 17:42:44 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
