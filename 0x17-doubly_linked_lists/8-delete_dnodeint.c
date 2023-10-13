ac9a35e19d7013ff9354baa2480a91b8f1a89746
techietrent07 <augustine.onu.200506@unn.edu.ng>
2023-10-12 22:03:41 -0700

    Delete at index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - A function that returns the number of
+ * elements in linked list
+ * @h: points to head of the linked list
+ * Return: the number of elements in the linked list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	int d = 0;
+
+	while (current)
+	{
+		d++;
+		current = current->next;
+	}
+
+	return (d);
+}
+
+/**
+ * delete_dnodeint_at_index - A function that deletes the node at
+ * index of dlistint_t linked list
+ * @head: A pointer to head of node
+ * @index: index to be deleted
+ * Return: 1 if successful, -1 if fail.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *tmp;
+	unsigned int i, len;
+
+	len = dlistint_len(*head);
+	if (index > len || len == 0)
+		return (-1);
+	current = *head;
+	if (len > 1 && index != 0)
+	{
+		if (current->next->next == NULL)
+		{
+			tmp = current->next;
+			current->prev = NULL;
+			current->next = NULL;
+			free(tmp);
+			return (1);
+		}
+	}
+	if (len == 1 && index == 0)
+	{
+		*head = NULL;
+		free(current);
+		return (1);
+	}
+	if (index == 0)
+	{
+		*head = current->next;
+		current->next->prev = NULL;
+		free(current);
+	}
+	else
+	{
+		for (i = 1; i < index; i++)
+			current = current->next;
+		tmp = current->next;
+		current->next->next->prev = current;
+		current->next = current->next->next;
+		free(tmp);
+	}
+	return (1);
+}
